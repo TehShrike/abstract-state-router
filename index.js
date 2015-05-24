ce879e04be3dfa32ce177e662391f5847833ea81
Josh Duff <me@JoshDuff.com>
2015-05-24 09:27:11 -0500

    Rolling back flipping the route evaluation order

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = function StateProvider(makeRenderer, rootElement, stateRouterOp
 	}, stateRouterOptions)
 
 	if (!stateRouterOptions.router) {
-		stateRouterOptions.router = newHashBrownRouter({ reverse: true })
+		stateRouterOptions.router = newHashBrownRouter()
 	}
 
 	current.set('', {})
