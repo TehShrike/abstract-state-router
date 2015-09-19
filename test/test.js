d2d64faa7853d594e3cdcaab45770b5ec3cd7892
Josh Duff <me@JoshDuff.com>
2015-09-19 12:49:06 -0500

    Switching to native-promise-only

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var test = require('tape-catch')
 var assertingRendererFactory = require('./helpers/asserting-renderer-factory')
 var getTestState = require('./helpers/test-state-factory')
-var Promise = require('pinkie-promise')
+var Promise = require('native-promise-only/npo')
 
 test('normal, error-less state activation flow for two states', function(t) {
 	function basicTest(t) {
