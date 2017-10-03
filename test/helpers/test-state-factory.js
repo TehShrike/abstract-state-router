77a5c82da2289119b301cf5c44c3c2d13e133cf0
Josh Duff <me@JoshDuff.com>
2017-10-03 13:11:30 -0500

    Remove a wild xtend reference in a test

--- test/helpers/test-state-factory.js
+++ test/helpers/test-state-factory.js
@@ -3,12 +3,11 @@ const hashLocationMockFactory = require('hash-brown-router/hash-location-mock')
 const stateRouterFactory = require('../../')
 const defaultRouterOptions = require('../../default-router-options.js')
 const mockRenderFn = require('./renderer-mock')
-const extend = require('xtend')
 
 module.exports = function getTestState(t, renderFn, options) {
 	const location = hashLocationMockFactory()
 	const hashRouter = hashRouterFactory(defaultRouterOptions, location)
-	const stateRouter = stateRouterFactory(renderFn || mockRenderFn, 'body', extend({
+	const stateRouter = stateRouterFactory(renderFn || mockRenderFn, 'body', Object.assign({
 		router: hashRouter,
 		throwOnError: false,
 	}, options))
