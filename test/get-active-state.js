86a30a8057a10b5e1fec2ffc2b14bcb3ba7f9106
Josh Duff <me@JoshDuff.com>
2018-08-03 15:53:25 -0500

    Slight changes to assertions, plus some autoformatter noise

--- test/get-active-state.js
+++ test/get-active-state.js
@@ -1,61 +1,60 @@
-const test = require('tape-catch')
-const getTestState = require('./helpers/test-state-factory')
+const test = require(`tape-catch`)
+const getTestState = require(`./helpers/test-state-factory`)
 
-test('getActiveState', t => {
-	const stateRouter = getTestState(t).stateRouter;
+test(`getActiveState with no parameters`, t => {
+	const stateRouter = getTestState(t).stateRouter
 
 	stateRouter.addState({
-		name: 'parent',
-		template: '',
-		route: '/parent',
+		name: `parent`,
+		template: ``,
+		route: `/parent`,
 	})
 
 	stateRouter.addState({
-		name: 'parent.child',
-		template: '',
-		route: '/child',
+		name: `parent.child`,
+		template: ``,
+		route: `/child`,
 	})
 
 	stateRouter.addState({
-		name: 'parent.child.grandchild',
-		template: '',
-		route: '/grandchild',
+		name: `parent.child.grandchild`,
+		template: ``,
+		route: `/grandchild`,
 	})
 
-	stateRouter.on('stateChangeEnd', () => {
-		t.ok(stateRouter.getActiveState().name === 'parent.child', 'child is active')
-		t.notOk(stateRouter.getActiveState() === 'parent', 'parent is not active')
-		t.notOk(stateRouter.getActiveState() === 'parent.child.grandchild', 'grandchild is not active')
+	stateRouter.on(`stateChangeEnd`, () => {
+		t.deepEqual(stateRouter.getActiveState(), {
+			name: `parent.child`,
+			parameters: {},
+		})
 
 		t.end()
 	})
 
-	stateRouter.go('parent.child')
+	stateRouter.go(`parent.child`)
 })
 
 
-test('getActiveState returns parameters', t => {
-	const stateRouter = getTestState(t).stateRouter;
+test(`getActiveState returns parameters`, t => {
+	const stateRouter = getTestState(t).stateRouter
 
 	stateRouter.addState({
-		name: 'parent',
-		template: '',
-		route: '/parent',
+		name: `parent`,
+		template: ``,
+		route: `/parent`,
 	})
 
 	stateRouter.addState({
-		name: 'parent.child',
-		template: '',
-		route: '/child',
+		name: `parent.child`,
+		template: ``,
+		route: `/child`,
 	})
 
-	stateRouter.on('stateChangeEnd', () => {
-		const params = stateRouter.getActiveState().parameters;
-		t.ok(params.butts === 'yes')
-		t.notOk(params.butts === 'no')
-		t.notOk(!params.butts)
+	stateRouter.on(`stateChangeEnd`, () => {
+		t.equal(stateRouter.getActiveState().parameters.butts, `yes`)
+
 		t.end()
 	})
 
-	stateRouter.go('parent.child', {butts: 'yes'})
+	stateRouter.go(`parent.child`, { butts: `yes` })
 })
