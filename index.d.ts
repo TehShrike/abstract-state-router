95fa474fc593538d24411c6c88aa9016414fdd89
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2019-10-14 19:35:24 +0700

    Add typescript type

---
+++ index.d.ts
@@ -0,0 +1,134 @@
+import EventEmitter from 'eventemitter3';
+
+type Parameter = {[key: string]: any};
+
+export interface Renderer<TemplateInputType, ElementType, TemplateType> {
+	render(
+		context: {
+			template: TemplateInputType;
+			element: ElementType;
+			content: any;
+			parameters: Parameter;
+		},
+		callback: (err: any, output: TemplateType) => void
+	): void;
+	reset(
+		context: {
+			domApi: TemplateType;
+			content: any;
+			template: TemplateInputType;
+			parameters: Parameter;
+		},
+		callback: (err: any, output: TemplateType) => void
+	): void;
+	destroy(
+		context: {
+			domApi: TemplateType;
+		},
+		callback: (err: any, output: TemplateType) => void
+	): void;
+	getChildElement(
+		domApi: TemplateType,
+		cb: (err: any, element: ElementType) => void
+	): void;
+}
+
+export interface State {
+	name: string;
+	parameters: Parameter;
+}
+
+export interface RouterOptions {
+	pathPrefix?: string;
+	router?: any; // FIXME
+	throwOnError?: boolean;
+}
+
+export interface StateResolveCallback<ContentType> {
+	(err: any, content: ContentType): void;
+	redirect(stateName: string, parameters?: Parameter): void;
+}
+
+export interface StateOption<
+	TemplateInputType,
+	TemplateType,
+	DataType,
+	ContentType
+> {
+	name: string;
+	template: TemplateInputType;
+	route?: string;
+	defaultChild?: string | (() => string);
+	data?: DataType;
+	querystringParameters?: string[];
+	defaultParameters?: Parameter;
+	defaultQuerystringParameters?: Parameter;
+
+	resolve?(
+		data: DataType,
+		parameters: Parameter,
+		callback: StateResolveCallback<ContentType>
+	): void | Promise<never>;
+	activate?(context: {
+		domApi: TemplateType;
+		data: DataType;
+		parameters: Parameter;
+		content: ContentType;
+	}): void;
+}
+
+export interface GoOption {
+	replace?: boolean;
+	inherit?: boolean;
+}
+
+type RouterEvents = {
+	stateChangeAttempt: [Function];
+	stateChangeStart: [State, Parameter, State[]];
+	stateChangeCancelled: [any];
+	stateChangeEnd: [State, Parameter, State[]];
+	stateChangeError: [any];
+	stateError: [any];
+	routeNotFound: [string, Parameter];
+};
+
+export class Router<TemplateInputType, TemplateType> extends EventEmitter<
+	RouterEvents
+> {
+	addState<DataType, ContentType>(
+		stateOption: StateOption<
+			TemplateInputType,
+			TemplateType,
+			DataType,
+			ContentType
+		>
+	): void;
+	go(
+		stateName: string,
+		stateParameters?: Parameter,
+		options?: GoOption
+	): void;
+	evaluateCurrentRoute(
+		fallbackStateName: string,
+		fallbackStateParameters?: Parameter
+	): void;
+	stateIsActive(stateName: string, stateParameters?: Parameter): boolean;
+	makePath(
+		stateName?: string,
+		stateParameters?: Parameter,
+		options?: {
+			inherit?: boolean;
+		}
+	): string;
+	getActiveState(): State;
+}
+
+export default function createStateRouter<
+	TemplateInputType,
+	ElementType,
+	TemplateType
+>(
+	makeRenderer: Renderer<TemplateInputType, ElementType, TemplateType>,
+	rootElement: ElementType,
+	options?: RouterOptions
+): Router<TemplateInputType, TemplateType>;
