6157a29bf5f363432708ea0863d223fd3d0273ab
TehShrike <me@JoshDuff.com>
2021-07-13 11:21:57 -0500

    Drop babel+ES5

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
 import visualizer from 'rollup-plugin-visualizer'
 
 export default {
-	input: './index.js',
+	input: `./index.js`,
 	output: [
 		{
-			file: './bundle.js',
-			format: 'cjs',
+			file: `./bundle.js`,
+			format: `cjs`,
 			sourcemap: true,
-			exports: 'default'
+			exports: `default`,
 		},
 	],
 	plugins: [
@@ -18,18 +17,15 @@ export default {
 		resolve({
 			browser: true,
 		}),
-		babel({
-			babelHelpers: 'bundled',
-		}),
 		visualizer(),
 	],
 	external: [
-		'combine-arrays',
-		'eventemitter3',
-		'hash-brown-router',
-		'iso-next-tick',
-		'page-path-builder',
-		'path-to-regexp-with-reversible-keys',
-		'then-denodeify',
+		`combine-arrays`,
+		`eventemitter3`,
+		`hash-brown-router`,
+		`iso-next-tick`,
+		`page-path-builder`,
+		`path-to-regexp-with-reversible-keys`,
+		`then-denodeify`,
 	],
 }
