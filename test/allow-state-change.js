1d3a454588c560cc8cf1054b3f149b743eaa5132
Charles Kaup <cv.kaup@gmail.com>
2023-06-09 18:46:45 -0500

    Add test case for `allowStateChange`
    Make sure it only gets called once when we're getting sent to a child case

--- test/allow-state-change.js
+++ test/allow-state-change.js
@@ -159,9 +159,7 @@ test(`allowStateChange can access domApi`, t => {
 				return true
 			},
 			resolve() {
-				return Promise.resolve({
-					foo: `bar`,
-				})
+				return Promise.resolve()
 			},
 			activate(context) {
 				//
@@ -200,6 +198,91 @@ test(`allowStateChange can access domApi`, t => {
 				t.end()
 			}
 		})
+	})
+
+	// TODO: I'm not sure how to write a test for the hashRouter
+})
+
+test(`allowStateChange will only fire once when getting redirected to a child`, t => {
+	function startTest(t) {
+		const state = getTestState(t)
+		const stateRouter = state.stateRouter
+		let allowStateChangeCalls = 0
+		t.plan(1)
+
+		stateRouter.addState({
+			name: `start`,
+			route: `/start`,
+			template: {},
+			allowStateChange: () => {
+				t.ok(allowStateChangeCalls === 0, `allowStateChange called`)
+				allowStateChangeCalls++
+				return true
+			},
+			resolve() {
+				return Promise.resolve()
+			},
+			activate(context) {
+			},
+		})
+
+		stateRouter.addState({
+			name: `end`,
+			route: `/end`,
+			defaultChild: `child`,
+			template: {},
+			resolve() {
+				return Promise.resolve()
+			},
+			activate() {
+			},
+		})
+
+		stateRouter.addState({
+			name: `end.child`,
+			route: `/child`,
+			template: {},
+			resolve() {
+				return Promise.resolve()
+			},
+			activate() {
+				t.end()
+			},
+		})
+
+		return state
+	}
+
+	t.test(`with state.go and applying default child`, t => {
+		const stateRouter = startTest(t).stateRouter
+		let arrivedAtStart = false
+
+		stateRouter.on(`stateChangeEnd`, () => {
+			const stateName = stateRouter.getActiveState().name
+
+			if (stateName === 'start' && !arrivedAtStart) {
+				arrivedAtStart = true
+				stateRouter.go(`end`)
+			}
+		})
+
+		stateRouter.go(`start`)
+	})
+
+	t.test(`with state.go and going directly to child`, t => {
+		const stateRouter = startTest(t).stateRouter
+		let arrivedAtStart = false
+
+		stateRouter.on(`stateChangeEnd`, () => {
+			const stateName = stateRouter.getActiveState().name
+
+			if (stateName === 'start' && !arrivedAtStart) {
+				arrivedAtStart = true
+				stateRouter.go(`end.child`)
+			}
+		})
+
+		stateRouter.go(`start`)
 	})
 
 	// TODO: I'm not sure how to write a test for the hashRouter
